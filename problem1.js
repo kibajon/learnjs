490a424633cf6b45cc41f94a237ee591a65e4bdc
Yokub <kiba_94_94@mail.ru>
2013-09-07 15:20:45 +0300

    java-script file

---
+++ problem1.js
@@ -0,0 +1,14 @@
+// Problem 1
+// Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// The function below returns the sum of 3 or 5 multiples between two numbers
+
+function calculateSum(min, max) {
+    
+    // TODO: should return sum of 3 or 5 multiples between min and max
+    return 0;
+}
+
+// test
+//calculateSum (0, 10) == 23
+//calculateSum (-10, 10) == 0
