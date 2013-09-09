0a190fe58c48e0f23aebaab10743478e03bc482e
Yokub <kiba_94_94@mail.ru>
2013-09-10 00:24:06 +0300

    I have done my homework!

--- problem1.js
+++ problem1.js
@@ -12,3 +12,37 @@ function calculateSum(min, max) {
 // test
 //calculateSum (0, 10) == 23
 //calculateSum (-10, 10) == 0
+
+
+//Done with For loop
+function calculateSum(min, max) {
+    
+    // we will keep track of the total here
+    var total = 0;
+    
+    // for every number between min and max
+    // check if it is divisible by 3 or 5
+    for (var i = min ; i < max; i++) {
+        
+        // if a number is divisible by 3 or 5, then add it to the total
+        if (i % 3 === 0 || i % 5 === 0)
+            total = total + i;
+    }
+    return total;
+}
+
+// test
+calculateSum (0, 10);
+
+//Done with While loop
+function calculateSum(min, max) {
+    var total = 0;
+    var i = min;
+    while(i<max){
+        if(i % 3 === 0 || i % 5 === 0)
+            total = total + i;
+            i ++;
+    }
+        return total;
+    }
+    calculateSum(0,10);
