0171ad5bdec89e2f0cee5a4916a6ce9d31543a3d
Yokub <kiba_94_94@mail.ru>
2013-09-12 21:42:06 +0300

    My second homework in js

---
+++ problem2.js
@@ -0,0 +1,81 @@
+// kilos to pounds
+// 1 kg = 2.20462 pounds
+
+function kilosToPounds (kilos) {
+	// TODO: return number of pounds for the given "kilos"
+	return 0;
+}
+
+// Test
+// kilosToPounds(5) == 11.0231
+
+function poundsToKilos (pounds) {
+	// TODO: return number of kilos for the given "pounds"
+	return 0;
+}
+
+// Test
+// poundsToKilos(20) == 9.07185
+
+
+// meters to feet
+// 1 meter  = 3.28084 feet
+
+function meterToFeet(meter) {
+	return 0;
+}
+
+// Test
+// meterToFeet(5) == 16.4042
+
+function feetToMeter (feet) {
+	return 0;
+}
+
+// Test
+// feetToMeter(20) == 6.096
+
+1) from kilos to pounds ; from pounds to kilos : // kilos to pounds
+// 1 kg = 2.20462 pounds
+
+function kilosToPounds (kilos) {
+    convertedMass = kilos * 2.20462;
+    // TODO: return number of pounds for the given "kilos"
+	return convertedMass;
+}
+
+// Test
+kilosToPounds(5);
+
+// pounds to kilos
+
+function poundsToKilos (pounds) {
+    convertedMass = pounds / 2.20462;
+	// TODO: return number of kilos for the given "pounds"
+	return convertedMass;
+}
+
+// Test
+poundsToKilos(20);
+
+
+2) from meters to feet ; from feet to meters : 
+// meters to feet
+// 1 meter  = 3.28084 feet
+
+function meterToFeet(meter) {
+    converetedDistance = meter * 3.28084; 
+	return converetedDistance;
+}
+
+// Test
+meterToFeet(5) == 16.4042
+
+// feet to meters
+function feetToMeter (feet) {
+    convertedDistance = feet / 3.28084;
+	return convertedDistance;
+}
+
+// Test
+feetToMeter(20);
